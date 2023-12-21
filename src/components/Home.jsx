ab165c82dc6d0498fe5fff2bf7d8e9f3f089a355
marcopoltergeist <paul_dubor@outlook.fr>
2023-12-21 16:50:30 +0100

    update card Netflex

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import '../styles/Home.scss'
+
+export default function Home() {
+    return <div>
+        <header className="header">
+        </header>
+        <h1>Netflex</h1>
+        <div className = "buttonTxt">
+            <p>Le netflix des pauvres pour profiter de vos films préférés sans image </p>
+            <button>Clique ici</button>
+        </div>
+        </div>
+}
